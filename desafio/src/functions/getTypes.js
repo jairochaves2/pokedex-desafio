a7a63b5aa5f566742759b06a74de262c3162eca4
José Maria Jairo Chaves <jairochaves2@gmail.com>
2021-02-02 16:45:35 +0000

    Update desafio/.gitignore, desafio/src/functions/getTypes.js, desafio/src/functions/pokemons.js files

---
+++ desafio/src/functions/getTypes.js
@@ -0,0 +1,6 @@
+const getTypes = async () => {
+ //Busca todos os os tipos de pokemons
+ //https://pokeapi.co/api/v2/type/
+};
+
+export default getTypes;
---
+++ desafio/src/functions/pokemons.js
@@ -0,0 +1,7 @@
+
+const getPokemonsByPage = async (page = 0) => {
+  //Link para carregar dados
+  //https://pokeapi.co/api/v2/pokemon?limit=100&offset=200
+};
+
+export default getPokemonsByPage;
