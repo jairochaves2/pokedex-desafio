9a4a2a260555567056fe3f93b6e4eb7d5c7189db
Jairo Chaves <jairochaves2@gmail.com>
2021-10-07 09:00:44 -0300

    Update getTypes.js

--- src/functions/getTypes.js
+++ src/functions/getTypes.js
@@ -1,6 +1,8 @@
 const getTypes = async () => {
- //Busca todos os os tipos de pokemons
- //https://pokeapi.co/api/v2/type/
+ //A função deve buscar e retornar todos os os tipos de pokemons
+ 
+  //Rota que que retorna os tipos de pokemon: 
+  //https://pokeapi.co/api/v2/type/
 };
 
 export default getTypes;
